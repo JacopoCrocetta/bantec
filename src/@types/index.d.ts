bcfcce7f12fe19319906f2f7021d9a491e2cff82
Jacopo <jacopo.crocetta@urbanasmart.com>
2023-10-26 16:34:35 +0200

    Added two new conf

---
+++ src/@types/index.d.ts
@@ -0,0 +1,63 @@
+export interface IConfigFile {
+  nodeEnv: string;
+  port: string;
+  jwt_encryption: string;
+  jwt_expiration: number;
+  apiKey: string;
+  logging: {
+    level: string;
+    useFileAppender: boolean;
+    prefix: string;
+    logsFolder: string;
+    fileName: string;
+    maxFiles: number;
+    maxSize: number;
+  };
+  infraServices: {
+    jwtToken: {
+      email: string;
+      password: string;
+      audience: string;
+    };
+    productionSoftware: {
+      username: string;
+      password: string;
+    };
+  };
+  mongo: {
+    debug: boolean;
+    address: string;
+    database: string;
+    user: string;
+    password: string;
+    maxConnectionAttempt: number;
+    connectionRetryTimeout: number;
+  };
+  db: {
+    db_dialect: any;
+    db_name: string;
+    db_user: string;
+    db_password: string;
+    db_options?: {
+      host?: string;
+      port: number;
+      readHost?: string;
+      writeHost?: string;
+    };
+  };
+  redis: {
+    keyPrefixPermission: string;
+    keyPrefix: string;
+    cacheDisabled: boolean;
+    options: {
+      password: string;
+      name: string;
+      sentinels: [
+        {
+          host: string;
+          port: number;
+        }
+      ];
+    };
+  };
+}
